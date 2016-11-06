f83f545de7c4aa40f40e624f50c0303a5cb81928
boris <borispovod@tutanota.com>
2016-11-06 05:03:09 +0300

    Added image, renamed package

--- angular-dropdowns.js
+++ angular-dropdowns.js
@@ -7,9 +7,9 @@
   var dd = angular.module('ngDropdowns', []);
 
   dd.run(['$templateCache', function ($templateCache) {
-    $templateCache.put('ngDropdowns/templates/dropdownSelect.html', [
+    $templateCache.put('ngDropdowns/templates/dropdownSelectImage.html', [
       '<div ng-class="{\'disabled\': dropdownDisabled}" class="wrap-dd-select" tabindex="0">',
-      '<span class="selected">{{dropdownModel[labelField]}}</span>',
+      '<img class="selected-img" ng-if="dropdownModel && dropdownModel.image"><span class="selected">{{dropdownModel[labelField]}}</span>',
       '<ul class="dropdown">',
       '<li ng-repeat="item in dropdownSelect"',
       ' class="dropdown-item"',
@@ -20,47 +20,19 @@
       '</div>'
     ].join(''));
 
-    $templateCache.put('ngDropdowns/templates/dropdownSelectItem.html', [
+    $templateCache.put('ngDropdowns/templates/dropdownSelectItemImage.html', [
       '<li ng-class="{divider: (dropdownSelectItem.divider && !dropdownSelectItem[dropdownItemLabel]), \'divider-label\': (dropdownSelectItem.divider && dropdownSelectItem[dropdownItemLabel])}">',
       '<a href="" class="dropdown-item"',
       ' ng-if="!dropdownSelectItem.divider"',
       ' ng-href="{{dropdownSelectItem.href}}"',
       ' ng-click="selectItem()">',
-      '{{dropdownSelectItem[dropdownItemLabel]}}',
+      '<img class="item-image" ng-if="dropdownSelectItem.image" ng-src="{{dropdownSelectItem.image}}"><span class="item">{{dropdownSelectItem[dropdownItemLabel]}}</span>',
       '</a>',
       '<span ng-if="dropdownSelectItem.divider">',
       '{{dropdownSelectItem[dropdownItemLabel]}}',
       '</span>',
       '</li>'
     ].join(''));
-
-    $templateCache.put('ngDropdowns/templates/dropdownMenu.html', [
-      '<ul class="dropdown">',
-      '<li ng-repeat="item in dropdownMenu"',
-      ' class="dropdown-item"',
-      ' dropdown-item-label="labelField"',
-      ' dropdown-menu-item="item">',
-      '</li>',
-      '</ul>'
-    ].join(''));
-
-    $templateCache.put('ngDropdowns/templates/dropdownMenuItem.html', [
-      '<li ng-class="{divider: dropdownMenuItem.divider, \'divider-label\': dropdownMenuItem.divider && dropdownMenuItem[dropdownItemLabel]}">',
-      '<a href="" class="dropdown-item"',
-      ' ng-if="!dropdownMenuItem.divider"',
-      ' ng-href="{{dropdownMenuItem.href}}"',
-      ' ng-click="selectItem()">',
-      '{{dropdownMenuItem[dropdownItemLabel]}}',
-      '</a>',
-      '<span ng-if="dropdownMenuItem.divider">',
-      '{{dropdownMenuItem[dropdownItemLabel]}}',
-      '</span>',
-      '</li>'
-    ].join(''));
-
-    $templateCache.put('ngDropdowns/templates/dropdownMenuWrap.html',
-      '<div class="wrap-dd-menu" ng-class="{\'disabled\': dropdownDisabled}"></div>'
-    );
   }]);
 
   dd.directive('dropdownSelect', ['DropdownService',
@@ -102,7 +74,7 @@
             DropdownService.unregister($element);
           });
         }],
-        templateUrl: 'ngDropdowns/templates/dropdownSelect.html'
+        templateUrl: 'ngDropdowns/templates/dropdownSelectImage.html'
       };
     }
   ]);
@@ -126,89 +98,11 @@
           };
         },
 
-        templateUrl: 'ngDropdowns/templates/dropdownSelectItem.html'
+        templateUrl: 'ngDropdowns/templates/dropdownSelectItemImage.html'
       };
     }
   ]);
 
-  dd.directive('dropdownMenu', ['$parse', '$compile', 'DropdownService', '$templateCache',
-    function ($parse, $compile, DropdownService, $templateCache) {
-      return {
-        restrict: 'A',
-        replace: false,
-        scope: {
-          dropdownMenu: '=',
-          dropdownModel: '=',
-          dropdownItemLabel: '@',
-          dropdownOnchange: '&',
-          dropdownDisabled: '='
-        },
-
-        controller: ['$scope', '$element', function ($scope, $element) {
-          $scope.labelField = $scope.dropdownItemLabel || 'text';
-
-          var $template = angular.element($templateCache.get('ngDropdowns/templates/dropdownMenu.html'));
-          // Attach this controller to the element's data
-          $template.data('$dropdownMenuController', this);
-
-          var tpl = $compile($template)($scope);
-          var $wrap = $compile(
-            angular.element($templateCache.get('ngDropdowns/templates/dropdownMenuWrap.html'))
-          )($scope);
-
-          $element.replaceWith($wrap);
-          $wrap.append($element);
-          $wrap.append($template);
-
-          DropdownService.register(tpl);
-
-          this.select = function (selected) {
-            if (!angular.equals(selected, $scope.dropdownModel)) {
-                $scope.dropdownModel = selected;
-            }
-            $scope.dropdownOnchange({
-              selected: selected
-            });
-          };
-
-          $element.bind('click', function (event) {
-            event.stopPropagation();
-            if (!$scope.dropdownDisabled) {
-              DropdownService.toggleActive(tpl);
-            }
-          });
-
-          $scope.$on('$destroy', function () {
-            DropdownService.unregister(tpl);
-          });
-        }]
-      };
-    }
-  ]);
-
-  dd.directive('dropdownMenuItem', [
-    function () {
-      return {
-        require: '^dropdownMenu',
-        replace: true,
-        scope: {
-          dropdownMenuItem: '=',
-          dropdownItemLabel: '='
-        },
-
-        link: function (scope, element, attrs, dropdownMenuCtrl) {
-          scope.selectItem = function () {
-            if (scope.dropdownMenuItem.href) {
-              return;
-            }
-            dropdownMenuCtrl.select(scope.dropdownMenuItem);
-          };
-        },
-
-        templateUrl: 'ngDropdowns/templates/dropdownMenuItem.html'
-      };
-    }
-  ]);
 
   dd.factory('DropdownService', ['$document',
     function ($document) {
